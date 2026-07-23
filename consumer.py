9aa63de56940dd28ae659fa79ac94187b5d2494f
Justin <justin022306@gmail.com>
2026-07-23 02:04:55 -0400

    feat: jupyter notebook matplotlib visual

--- consumer.py
+++ consumer.py
@@ -15,12 +15,6 @@ class Event(BaseModel):
     itemid: int
     transactionid: int | None = None
 
-class Failed_Event(BaseModel):
-    id: int
-    raw_message: str
-    error: str
-    failed_at: int
-
 consumer = KafkaConsumer(
     "events",
     bootstrap_servers="localhost:19092",
@@ -44,20 +38,24 @@ with psycopg.connect("dbname=pulsegrid user=justin password=password host=localh
                 validate = Event.model_validate_json(message)
                 valid_events.append(validate)
             except ValidationError as e:
-                print(e) 
-                failed_events.append({"raw": message, "error": str(e)})
+                print(e)
+                failed = {"message": message, "error": str(e)}
+                failed_events.append(failed)
 
-            if len(valid_events) >= 100 or (time.time() - last_flush >= 10):
+            if len(valid_events) >= 100 or (time.time() - last_flush >= 10): 
                 if valid_events:
                     rows = [(e.timestamp, e.visitorid, e.event.value, e.itemid, e.transactionid) for e in valid_events]
                     cur.executemany("INSERT INTO event (timestamp, visitorid, event, itemid, transactionid) VALUES (%s, %s, %s, %s, %s)", rows)
                     conn.commit()
                     print(f"flushed {len(valid_events)} to postgres")
                     valid_events = []
-                if failed_events:
+                if failed_events: # dead letter queue
                     rows = [(f["raw"], f["error"]) for f in failed_events]
                     cur.executemany("INSERT INTO failed_events (raw_message, error) VALUES (%s, %s)", rows)
                     conn.commit()
                     print(f"flushed {len(failed_events)} to postgres")
                     failed_events = []
                 last_flush = time.time() # reset the time to 0 again
+
+
+# SELECT event, COUNT(*) AS TOTAL FROM event GROUP BY event ORDER BY total DESC;
